da69a20d2648b2c7d35e475afd91739a61013b87
NikitaToropov <nikkyniknikita@gmail.com>
2021-10-02 20:37:12 +0300

    добавил в метод вращение механизм изменения координат маленького куба

--- src/main/java/cube/SmallCube.java
+++ src/main/java/cube/SmallCube.java
@@ -1,7 +1,5 @@
 package cube;
 
-import com.sun.xml.internal.bind.v2.runtime.Coordinator;
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,8 +12,7 @@ public class SmallCube {
     public final Map<Sides, Sides> faces;
 
 
-    public SmallCube(Map<Sides, Sides> faces, Coordinate coordinate)
-    {
+    public SmallCube(Map<Sides, Sides> faces, Coordinate coordinate) {
         this.faces = faces;
         this.coordinate = coordinate;
     }
@@ -39,9 +36,10 @@ public class SmallCube {
             newFaces.put(Sides.U, face);
         }
 
-        int x = 
-
-        return new SmallCube(newFaces);
+        int x = coordinate.y;
+        int y = coordinate.x * -1;
+        int z = coordinate.z;
+        return new SmallCube(newFaces, new Coordinate(x, y, z));
     }
 
     public SmallCube rotateRight() {
@@ -63,7 +61,10 @@ public class SmallCube {
             newFaces.put(Sides.U, face);
         }
 
-        return new SmallCube(newFaces);
+        int x = coordinate.x;
+        int z = coordinate.y;
+        int y = coordinate.z * -1;
+        return new SmallCube(newFaces, new Coordinate(x, y, z));
     }
 
     public SmallCube rotateDown() {
@@ -85,7 +86,10 @@ public class SmallCube {
             newFaces.put(Sides.F, face);
         }
 
-        return new SmallCube(newFaces);
+        int x = coordinate.z * -1;
+        int y = coordinate.y;
+        int z = coordinate.x;
+        return new SmallCube(newFaces, new Coordinate(x, y, z));
     }
 
     public SmallCube rotateLeft() {
@@ -107,7 +111,10 @@ public class SmallCube {
             newFaces.put(Sides.F, face);
         }
 
-        return new SmallCube(newFaces);
+        int x = coordinate.x;
+        int y = coordinate.z;
+        int z = coordinate.y * -1;
+        return new SmallCube(newFaces, new Coordinate(x, y, z));
     }
 
     public SmallCube rotateUp() {
@@ -129,7 +136,10 @@ public class SmallCube {
             newFaces.put(Sides.F, face);
         }
 
-        return new SmallCube(newFaces);
+        int x = coordinate.z;
+        int y = coordinate.y;
+        int z = coordinate.x * -1;
+        return new SmallCube(newFaces, new Coordinate(x, y, z));
     }
 
     public SmallCube rotateBack() {
@@ -151,6 +161,9 @@ public class SmallCube {
             newFaces.put(Sides.R, face);
         }
 
-        return new SmallCube(newFaces);
+        int y = coordinate.x;
+        int x = coordinate.y * -1;
+        int z = coordinate.z;
+        return new SmallCube(newFaces, new Coordinate(x, y, z));
     }
 }

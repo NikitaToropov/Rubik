e59cfb7582bb2e097ac8924d4e17f650e2f634fc
NikitaToropov <nikkyniknikita@gmail.com>
2021-10-04 14:30:13 +0300

    перенес методы вращения большого куба в его абстракцию

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,12 @@
 import cube.BigCube;
 import utils.BigCubeGenerator;
 import utils.BigCubePrinter;
-import utils.BigCubeRotator;
 
 public class Main {
     public static void main(String[] args) {
         BigCube bigCube = BigCubeGenerator.generateBigCube();
         BigCubePrinter.print(bigCube);
-        BigCube another = BigCubeRotator.rotateBack(bigCube);
-        another = BigCubeRotator.rotateBack(another);
-        another = BigCubeRotator.rotateBack(another);
-        another = BigCubeRotator.rotateBack(another);
+        BigCube another = bigCube.rotateFront();
         BigCubePrinter.print(another);
 
         System.out.println(bigCube.equals(another));
--- src/main/java/cube/BigCube.java
+++ src/main/java/cube/BigCube.java
@@ -1,5 +1,8 @@
 package cube;
 
+import utils.BigCubeGenerator;
+
+import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
@@ -29,5 +32,77 @@ public class BigCube {
     @Override
     public int hashCode() {
         return Objects.hash(cubes);
+    }
+
+    public BigCube rotateLeft() {
+        HashSet<SmallCube> smallCubes = new HashSet<>();
+        cubes.forEach(c -> {
+            if (c.coordinate.x == BigCubeGenerator.minX) {
+                smallCubes.add(c.rotateLeft());
+            } else {
+                smallCubes.add(c);
+            }
+        });
+        return new BigCube(this, smallCubes, size);
+    }
+
+    public BigCube rotateFront() {
+        HashSet<SmallCube> smallCubes = new HashSet<>();
+        cubes.forEach(c -> {
+            if (c.coordinate.z == BigCubeGenerator.minZ) {
+                smallCubes.add(c.rotateFront());
+            } else {
+                smallCubes.add(c);
+            }
+        });
+        return new BigCube(this, smallCubes, size);
+    }
+
+    public BigCube rotateRight() {
+        HashSet<SmallCube> smallCubes = new HashSet<>();
+        cubes.forEach(c -> {
+            if (c.coordinate.x == BigCubeGenerator.maxX) {
+                smallCubes.add(c.rotateRight());
+            } else {
+                smallCubes.add(c);
+            }
+        });
+        return new BigCube(this, smallCubes, size);
+    }
+
+    public BigCube rotateBack() {
+        HashSet<SmallCube> smallCubes = new HashSet<>();
+        cubes.forEach(c -> {
+            if (c.coordinate.z == BigCubeGenerator.maxZ) {
+                smallCubes.add(c.rotateBack());
+            } else {
+                smallCubes.add(c);
+            }
+        });
+        return new BigCube(this, smallCubes, size);
+    }
+
+    public BigCube rotateUp() {
+        HashSet<SmallCube> smallCubes = new HashSet<>();
+        cubes.forEach(c -> {
+            if (c.coordinate.y == BigCubeGenerator.maxY) {
+                smallCubes.add(c.rotateUp());
+            } else {
+                smallCubes.add(c);
+            }
+        });
+        return new BigCube(this, smallCubes, size);
+    }
+
+    public BigCube rotateDown() {
+        HashSet<SmallCube> smallCubes = new HashSet<>();
+        cubes.forEach(c -> {
+            if (c.coordinate.y == BigCubeGenerator.minY) {
+                smallCubes.add(c.rotateDown());
+            } else {
+                smallCubes.add(c);
+            }
+        });
+        return new BigCube(this, smallCubes, size);
     }
 }

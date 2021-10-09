9fd5359ccf37802d8354a93f1993dea598e388c3
nikitatoropov <nikkyniknikita@gmail.com>
2021-10-09 16:19:48 +0300

    отформтировал Goal

--- src/main/java/enums/Goal.java
+++ src/main/java/enums/Goal.java
@@ -36,40 +36,40 @@ public enum Goal {
 
     private static boolean isSolvedGoal(Cube cube, Goal goal) {
         switch (goal) {
-            case CROSS_W: //754
+            case CROSS_W:
                 if (!CubeGoalRealisation.crossW(cube)) return false;
                 break;
-            case THIRD_LAYER_INCONSISTENT_CROSS_W: //781
+            case THIRD_LAYER_INCONSISTENT_CROSS_W:
                 if (!CubeGoalRealisation.inconsistentThirdLayerCrossU(cube)) return false;
                 break;
-            case THIRD_LAYER_CROSS_W: //784
+            case THIRD_LAYER_CROSS_W:
                 if (!CubeGoalRealisation.ThirdLayerCrossU(cube)) return false;
                 break;
-            case THIRD_LAYER_CORNER_POSITION_1_W: //787
+            case THIRD_LAYER_CORNER_POSITION_1_W:
                 if (!CubeGoalRealisation.ThirdLayerCornerPositionU1(cube)) return false;
                 break;
-            case THIRD_LAYER_CORNER_POSITIONS: //790
+            case THIRD_LAYER_CORNER_POSITIONS:
                 if (!CubeGoalRealisation.ThirdLayerCornerPositions(cube)) return false;
                 break;
-            case F2L1: //858
+            case F2L1:
                 if (!CubeGoalRealisation.F2L1(cube)) return false;
                 break;
-            case F2L2: //861
+            case F2L2:
                 if (!CubeGoalRealisation.F2L2(cube)) return false;
                 break;
-            case F2L3: //864
+            case F2L3:
                 if (!CubeGoalRealisation.F2L3(cube)) return false;
                 break;
-            case F2L4: //867
+            case F2L4:
                 if (!CubeGoalRealisation.F2L4(cube)) return false;
                 break;
-            case THIRD_LAYER_PARTIAL_CONSISTENT_CROSS: //870
+            case THIRD_LAYER_PARTIAL_CONSISTENT_CROSS:
                 if (!CubeGoalRealisation.thirdLayerPartialConsistentCross(cube)) return false;
                 break;
-            case THIRD_LAYER_PARTIAL_INCONSISTENT_CROSS: //873
+            case THIRD_LAYER_PARTIAL_INCONSISTENT_CROSS:
                 if (!CubeGoalRealisation.thirdLayerPartialInconsistentCross(cube)) return false;
                 break;
-            case THIRD_LAYER_1_CORNER: //876
+            case THIRD_LAYER_1_CORNER:
                 if (!CubeGoalRealisation.thirdLayer1Corner(cube)) return false;
                 break;
             case SOLVED:
